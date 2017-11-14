1083a017de68a6f9a6fa6ad39806d0f1f6ef1596
Edgardo Mendez-Soto <gwenchy15@gmail.com>
2017-11-14 11:15:26 -0500

    Finished some html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+    // Variables
+    var yourCharacter = false;
+    var yourEnemies = false;
+    var yourDefender = false;
+    var playersArray = [];
+    var enemyDefeatedCount = 0;
+
+
+    // Characters stats
+
+    var firstPlayer = {
+        name: "Wargreymon",
+        healthPoints: 120,
+        attackPower: 10,
+        counterAttackPower: 20,
+        numberOfAttacks: 1
+
+    }
+
+    var secondPlayer = {
+        name: 'Metalgarurumon',
+        healthPoints: 160,
+        attackPower: 7,
+        counterAttackPower: 15,
+        numberOfAttacks: 1
+    }
+
+    var thirdPlayer = {
+        name: "Omnimon",
+        healthPoints: 170,
+        attackPower: 14,
+        counterAttackPower: 25,
+        numberOfAttacks: 1
+    }
+
+    var fourthPlayer = {
+        name: "Agumon",
+        healthPoints: 100,
+        attackPower: 4,
+        counterAttackPower: 10,
+        numberOfAttacks: 1
+    }
+
+    playersArray.push(firstPlayer);
+    playersArray.push(secondPlayer);
+    playersArray.push(thirdPlayer);
+    playersArray.push(fourthPlayer);
+
+    //  Restart game
+
+    function startGame() {
+        yourCharacter = false;
+        yourEnemies = false;
+        yourDefender = false;
+    };
+
+    // Background music
+
+    var audio = new Audio("assets/images/Music.mp3");
+
+    audio.play();
+
+    // Choosing character process.
+    function slectContestants(event) {
+        event.preventDefaults();
+        var $this = $(this);
+
+        
+    }
+});
